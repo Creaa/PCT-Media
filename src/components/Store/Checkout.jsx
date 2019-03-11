3c2f6b87dc7dfb329ea89ad365d7c9c7cb427cdf
Sebastian Kondratowicz <feeverpl@gmail.com>
2019-03-11 16:54:16 +0100

    form added

--- src/components/Store/Checkout.jsx
+++ src/components/Store/Checkout.jsx
@@ -12,7 +12,8 @@ class Checkout extends Component {
 			streetInput: '',
 			houseNumberInput: '',
 			cityInput: '',
-			postCodeInput: ''
+			postCodeInput: '',
+			emailCorrect: false,
 
 		}
 	}
@@ -27,6 +28,77 @@ class Checkout extends Component {
 	// 		this.calculateTotalPrice()
 	// 	}, 1);
 	// }
+
+	submitHandler = () => {
+		if (this.state.firstNameInput.length > 0 && this.state.lastNameInput.length > 0 && this.state.emailCorrect && this.state.streetInput.length > 0 && this.state.houseNumberInput.length > 0 && this.state.cityInput.length > 0 && this.state.postCodeInput.length > 0) {
+			let productInfo = {
+				user: {
+					name: this.state.firstNameInput,
+					surname: this.state.lastNameInput,
+					email: this.state.emailInput,
+					address: {
+						street: this.state.streetInput,
+						houseNumber: this.state.houseNumberInput,
+						city: this.state.cityInput,
+						postcode: this.state.postCodeInput
+					}
+				},
+				product: this.props.products
+			}
+			console.log(productInfo)
+		}
+		else {
+			alert("Error, invalid form data!")
+		}
+	}
+
+	firstNameInputHandler = (e) => {
+		this.setState({
+			firstNameInput: e.target.value
+		})
+	}
+	lastNameInputHandler = (e) => {
+		this.setState({
+			lastNameInput: e.target.value
+		})
+	}
+	emailInputHandler = (e) => {
+		this.setState({
+			emailInput: e.target.value
+		})
+		let regExMail = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+		if (regExMail.test(e.target.value)) {
+			this.setState({
+				emailCorrect: true,
+			})
+		} else {
+			this.setState({
+				emailCorrect: false,
+			})
+		}
+	}
+	streetInputHandler = (e) => {
+		this.setState({
+			streetInput: e.target.value
+		})
+	}
+	houseNumberInputHandler = (e) => {
+		this.setState({
+			houseNumberInput: e.target.value
+		})
+	}
+	cityInputHandler = (e) => {
+		this.setState({
+			cityInput: e.target.value
+		})
+	}
+
+	postCodeInputHandler = (e) => {
+		this.setState({
+			postCodeInput: e.target.value
+		})
+	}
+
 
 	componentDidMount = () => {
 		let allProducts = ProductsList;
@@ -74,19 +146,20 @@ class Checkout extends Component {
 				<div className="ship-form">
 					<h5>Shipment Addres</h5>
 					<label>First Name</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.firstNameInputHandler} value={this.state.firstNameInput} className="w3-input form-input" type="text" />
 					<label>Last name</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.lastNameInputHandler} value={this.state.lastNameInput} className="w3-input form-input" type="text" />
 					<label>Email</label>
-					<input className="w3-input form-input" type="email" />
+					<input onChange={this.emailInputHandler} value={this.state.emailInput} className="w3-input form-input" type="email" />
 					<label>Street</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.streetInputHandler} value={this.state.streetInput} className="w3-input form-input" type="text" />
 					<label>House number</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.houseNumberInputHandler} value={this.state.houseNumberInput} className="w3-input form-input" type="text" />
 					<label>City</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.cityInputHandler} value={this.state.cityInput} className="w3-input form-input" type="text" />
 					<label>PostCode</label>
-					<input className="w3-input form-input" type="text" />
+					<input onChange={this.postCodeInputHandler} value={this.state.postCodeInput} className="w3-input form-input" type="text" />
+					<button onClick={this.submitHandler} className="store-button form-submit">Submit</button>
 				</div>
 			</div>
 		);
